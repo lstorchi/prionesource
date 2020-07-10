d9e9e4b6c635d66d6822efa02b435a1d7e8f2bc6
Loriano Storchi <loriano@storchi.org>
2020-07-10 16:57:05 +0200

    backup TODO

--- common/carbo.py
+++ common/carbo.py
@@ -52,9 +52,10 @@ def get_seps (refpoint, molcoord, molcharges, coulombconst):
 
   sum = 0.0
 
-  if (dist.min() > 1.0):
-    ep = coulombconst * (molcharges/dist)
-    sum = numpy.sum(ep) 
+  print("TODO")
+  #if (dist.min() > 1.0):
+  ep = coulombconst * (molcharges/dist)
+  sum = numpy.sum(ep) 
 
   return sum
 
