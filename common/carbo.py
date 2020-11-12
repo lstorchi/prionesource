accb491a5c155371aa929c20e9417f160df9c266
Loriano Storchi <loriano@storchi.org>
2020-11-12 12:58:23 +0100

    now working still some problem in z

--- common/carbo.py
+++ common/carbo.py
@@ -274,21 +274,54 @@ def returncarbodxs(set1, set2, verbose=False, axis="x"):
       except TypeError as te:
         raise Exception(v1 + " and " + v2 + " are not compatible ")
 
+      x = y = z = 0.0
+      ix = iy = iz = 0
+      movcoord = 0.0
+
       for i in range(g1.grid.shape[aidx1]):
-        x = g1.origin[aidx1] + i*g1.delta[aidx1]
+        if aidx1 == 0:
+          ix = i
+          x = g1.origin[aidx1] + i*g1.delta[aidx1]
+          movcoord = x
+        elif aidx1 == 1:
+          iy = i
+          y = g1.origin[aidx1] + i*g1.delta[aidx1]
+          movcoord = y
+        elif aidx1 == 2:
+          iz = i
+          z = g1.origin[aidx1] + i*g1.delta[aidx1]
+          movcoord = z
 
         num = 0.0
         denum1 = 0.0 
         denum2 = 0.0
-        
-        for j in range(g1.grid.shape[aidx2]):
-          y = g1.origin[aidx2] + j*g1.delta[aidx2]
-          for k in range(g1.grid.shape[aidx3]):
-            z = g1.origin[aidx3] + k*g1.delta[aidx3]
 
-            num = num + g1.grid[i, j, k]*g2.grid[i, j, k]
-            denum1 = denum1 + g1.grid[i, j, k]*g1.grid[i, j, k]
-            denum2 = denum2 + g2.grid[i, j, k]*g2.grid[i, j, k]
+        for j in range(g1.grid.shape[aidx2]):
+          if aidx2 == 0:
+            ix = j
+            x = g1.origin[aidx2] + j*g1.delta[aidx2]
+          elif aidx2 == 1:
+            iy = j
+            y = g1.origin[aidx2] + j*g1.delta[aidx2]
+          elif aidx2 == 2:
+            iz = j
+            z = g1.origin[aidx2] + j*g1.delta[aidx2]
+
+          for k in range(g1.grid.shape[aidx3]):
+            if aidx3 == 0:
+              ix = k
+              x = g1.origin[aidx3] + k*g1.delta[aidx3]
+            elif aidx3 == 1:
+              iy = k
+              y = g1.origin[aidx3] + k*g1.delta[aidx3]
+            elif aidx3 == 2:
+              iz = k
+              z = g1.origin[aidx3] + k*g1.delta[aidx3]
+
+            
+            num = num + g1.grid[ix, iy, iz]*g2.grid[ix, iy, iz]
+            denum1 = denum1 + g1.grid[ix, iy, iz]*g1.grid[ix, iy, iz]
+            denum2 = denum2 + g2.grid[ix, iy, iz]*g2.grid[ix, iy, iz]
         
         carboidx = 0.0
         if denum1 == 0.0 and denum2 != 0.0:
@@ -301,9 +334,9 @@ def returncarbodxs(set1, set2, verbose=False, axis="x"):
             carboidx = num/math.sqrt(denum1 * denum2)
 
         if verbose:
-          print("%10.5f %10.5f"%(x, carboidx)) 
+          print("%10.5f %10.5f"%(movcoord, carboidx)) 
 
-        xrefpoints[i] = x
+        xrefpoints[i] = movcoord
         carboidxs[generalidx,i] = carboidx
 
       generalidx += 1
